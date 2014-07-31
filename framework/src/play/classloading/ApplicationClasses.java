1060cfa29599ca0ee8eb1bac81670ac473c55ed1
Robert Rettig <rrettig@synapplix.de>
2014-07-31 07:27:20 +0200

    - merge case sensitivity for java types on windows

--- framework/src/play/classloading/ApplicationClasses.java
+++ framework/src/play/classloading/ApplicationClasses.java
@@ -3,6 +3,7 @@ package play.classloading;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -313,9 +314,11 @@ public class ApplicationClasses {
 
     // ~~ Utils
     /**
-     * Retrieve the corresponding source file for a given class name.
-     * It handles innerClass too !
-     * @param name The fully qualified class name 
+     * Retrieve the corresponding source file for a given class name. It handles
+     * innerClass too !
+     * 
+     * @param name
+     *          The fully qualified class name
      * @return The virtualFile if found
      */
     public static VirtualFile getJava(String name) {
@@ -323,11 +326,22 @@ public class ApplicationClasses {
         if (fileName.contains("$")) {
             fileName = fileName.substring(0, fileName.indexOf("$"));
         }
-        fileName = fileName.replace(".", "/") + ".java";
+        String fileOrDir = fileName.replace(".", "/");
+        fileName = fileOrDir + ".java";
         for (VirtualFile path : Play.javaPath) {
-            VirtualFile javaFile = path.child(fileName);
+            VirtualFile javaFile = path.child(fileOrDir);
+            if (javaFile.exists() && javaFile.isDirectory()) {
+                return null;
+            }
+            javaFile = path.child(fileName);
             if (javaFile.exists()) {
-                return javaFile;
+                try {
+                    if (!fileName.endsWith(javaFile.getRealFile().getCanonicalFile().getName())) {
+                        return null;
+                    }
+                } catch (IOException e) {
+                }
+              return javaFile;
             }
         }
         return null;
